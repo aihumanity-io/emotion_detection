a140edb1b4438b604cf92bcfbb209c68d3849eda
David Chiu <fdchiu@gmail.com>
2025-11-30 22:43:20 +0800

    chore: log user code/shard lengths during fetch setup

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:emotion_detection/emotion_detection.dart';
@@ -35,7 +36,7 @@ class _MyAppState extends State<MyApp> {
   static const bool _kOneTimeClearCaches = false;
   bool _didClearCaches = false;
 
-  static const Map<String, String> _modelAccountIds = {
+  static  Map<String, String> _modelAccountIds = {
     'aih_fer20250115': 'aih_fer20250115_v2025-01-15-shard',
     'mobilenetv1_fer2024-11-06-08-48-50':
         'mobilenetv1_fer2024-11-06-08-48-50_v2025-01-15-shard',
@@ -55,6 +56,13 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    if(Platform.isAndroid) {
+      _modelAccountIds = {
+        'mobilenetv1_fer2024-11-06-08-48-50':
+        'mobilenetv1_fer2024-11-06-08-48-50',
+      };
+    }
+
     initPlatformState();
   }
 
@@ -161,6 +169,10 @@ class _MyAppState extends State<MyApp> {
             final userCodeB64 = _sdkSecretModule.extractUserCode(res.payload);
             if (userCodeB64 != null) {
               try {
+                if (kDebugMode) {
+                  debugPrint(
+                      'Storing user code for $modelKey len=${userCodeB64.length}');
+                }
                 await UserCodeChannel.saveUserCode(
                   userName: _sdkSecretModule.userName,
                   userCodeB64: userCodeB64,
@@ -183,6 +195,10 @@ class _MyAppState extends State<MyApp> {
               );
               if (shardB64 != null) {
                 try {
+                  if (kDebugMode) {
+                    debugPrint(
+                        'Storing shard for $modelKey len=${shardB64.length} exp=$expiresAtMs');
+                  }
                   for (final id in _allModelIds(modelKey)) {
                     await ModelRuntime.setKeyShard(
                       modelId: id,
